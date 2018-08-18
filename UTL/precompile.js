1f139630283885913a6843bbac5d5f6bffbbd148
RZ <rzfang@gmail.com>
2018-08-18 23:49:48 +0800

    some update.

--- UTL/precompile.js
+++ UTL/precompile.js
@@ -2,12 +2,13 @@
 
 'use strict';
 
-var fs = require('fs'),
-    sass = require('node-sass'),
-    uglifyjs = require('uglify-js'),
-    RtPth = __dirname + '/../', // 'RtPth' = Root Path.
-    SrcPth = RtPth + 'SRC/', // 'SrcPth' = Source Path.
-    RscPth = RtPth + 'WEB/www/resource/'; // 'RscPth' = Resouce Path.
+const fs = require('fs'),
+      sass = require('node-sass'),
+      uglifyjs = require('uglify-js'),
+
+const RtPth = __dirname + '/../', // 'RtPth' = Root Path.
+      RscPth = RtPth + 'WEB/www/resource/', // 'RscPth' = Resouce Path.
+      SrcPth = RtPth + 'SRC/'; // 'SrcPth' = Source Path.
 
 SCSS_CSS(SrcPth + 'base.scss', RscPth + 'base.css');
 SCSS_CSS(SrcPth + 'style1.scss', RscPth + 'style1.css');
@@ -22,17 +23,15 @@ JsCompress(
 
 return 0;
 
-function SCSS_CSS (FrmPth, ToPth)
-{
-  var Src = fs.readFileSync(FrmPth, 'utf8'), // 'Src' = Source.
-      CSS = sass.renderSync({'data': Src}).css.toString().replace(/\n +/g, ' ').replace(/\n\n/g, "\n");
+function SCSS_CSS (FrmPth, ToPth) {
+  const Src = fs.readFileSync(FrmPth, 'utf8'), // 'Src' = Source.
+        CSS = sass.renderSync({ data: Src }).css.toString().replace(/\n +/g, ' ').replace(/\n\n/g, "\n");
 
   fs.writeFileSync(ToPth, CSS);
 }
 
-function JsCompress (FrmPthA, ToPth)
-{
-  var Js = uglifyjs.minify(FrmPthA, {'mangle': true}).code;
+function JsCompress (FrmPthA, ToPth) {
+  const Js = uglifyjs.minify(FrmPthA, { mangle: true }).code;
 
   fs.writeFileSync(ToPth, Js);
 }
