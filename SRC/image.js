d73518479062548548df88eb6f02c477eb6f41a4
RZ <rzfang@gmail.com>
2018-10-26 00:48:55 +0800

    add riot page files. add image handle. update packages.

---
+++ SRC/image.js
@@ -0,0 +1,75 @@
+const fs = require('fs'),
+      path = require('path');
+
+const Cnst = require('./constant.json');
+
+const CCH_PTH = path.resolve(__dirname, '..', Cnst.CCH_PTH), // cache files path.
+      MM_TP = {
+        '.bmp': 'image/x-windows-bmp',
+        '.gif': 'image/gif',
+        '.jpg': 'image/jpeg',
+        '.png': 'image/png',
+        '.svg': 'image/svg+xml' }; // mime type map.
+
+function CachedFileRespond (Rqst, Rspns, UrlInfo) {
+  let FlPth = UrlInfo.pathname.match(/^\/resource\/image\/(.+)/); // file name.
+
+  if (!FlPth || FlPth.length < 2) {
+    Rspns.writeHead(404, { 'Content-Type': 'text/plain' });
+    Rspns.write('');
+    Rspns.end();
+
+    return;
+  }
+
+  FlPth = CCH_PTH + '/' + decodeURI(FlPth[1]);
+
+  fs.stat(
+    FlPth,
+    (Err, St) => {
+      if (Err) {
+        Rspns.writeHead(
+          404,
+          { 'Content-Type': MM_TP[path.extname(FlPth)],
+            'Content-Length': 0 });
+        Rspns.write('');
+        Rspns.end();
+
+        return;
+      }
+
+      const Mms = St.mtimeMs || (new Date(St.mtime)).getTime(); // mtime milisecond.
+
+      if (Rqst.headers['if-modified-since'] && Rqst.headers['if-modified-since'] !== 'Invalid Date') {
+        const ChkdMs = (new Date(Rqst.headers['if-modified-since'])).getTime(); // checked milisecond.
+
+        if (Mms < ChkdMs) {
+          Rspns.writeHead(
+            304,
+            { 'Content-Type': MM_TP[path.extname(FlPth)],
+              'Cache-Control': 'public, max-age=6000',
+              'Last-Modified': Rqst.headers['if-modified-since'] });
+
+          Rspns.write('\n');
+          Rspns.end();
+
+          return;
+        }
+      }
+
+      const RdStrm = fs.createReadStream(FlPth); // ready stream.
+
+      Rspns.writeHead(
+        200,
+        { 'Content-Type': MM_TP[path.extname(FlPth)],
+          'Cache-Control': 'public, max-age=6000',
+          'Last-Modified': (new Date(Mms + 1000)).toUTCString() });
+
+      RdStrm.pipe(Rspns);
+    });
+}
+
+module.exports = {
+  CachedFileRespond
+};
+
---
+++ SRC/page/text.tag
@@ -0,0 +1,55 @@
+<text>
+  <h1>{Ttl}</h1>
+  <div>
+    <tags tgs={Tgs}/>
+    <icon title='發佈時間' nm='calendar'/> {Dt}
+  </div>
+  <pre>{Str}</pre>
+  <share-box ecdurl={Url}/>
+  <style scoped>
+    :scope { display: block; }
+    :scope>div { margin: 20px 0; }
+    :scope>pre { line-height: 2rem; }
+  </style>
+  <script>
+    this.Ttl = '';
+    this.Tgs = [];
+    this.Dt = '';
+    this.Str = '';
+    this.Url = '';
+
+    this.mixin('Z.RM');
+
+    this.StoreListen(
+      'BLOG',
+      (Sto, Prms) => {
+        this.update({ Ttl: Sto.Ttl, Tgs: Sto.Tgs, Dt: Sto.Dt, Str: Sto.Info, Url: encodeURI(Sto.Url) });
+      });
+
+    this.on(
+      'mount',
+      () => {
+        if (!this.OnBrowser()) { return; }
+
+        const Id = window.location.search.replace('?b=', '');
+
+        if (!Id) {
+          window.location = '/404';
+
+          return;
+        }
+
+        this.ServiceCall(
+          '/service/blog',
+          { Id },
+          'BLOG',
+          (Sto, Rst) => {
+            if (!Rst) { return alert(window.Z.Kwd.SystemError); }
+
+            if (Rst.Index < 0) { return alert(Rst.Message); }
+
+            return Rst.Extend;
+          });
+      });
+  </script>
+</text>
