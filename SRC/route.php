0d1764cfdfde0419276979bd0b902ca85711bc42
RZ <rzfang@gmail.com>
2019-08-12 01:15:24 +0800

    PHP, thank you, farewell.

--- SRC/route.php
+++
@@ -1,8 +0,0 @@
-<?php
-  if (preg_match('/\.(js|css)$/', $_SERVER['REQUEST_URI'])) {
-    header('Location: http://localhost:9004' . $_SERVER['REQUEST_URI']);
-    die();
-  }
-
-  return false;
-?>
